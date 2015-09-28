31e8d8cc79bf62aeec79e6fdeeb5a2f6de896a63
klermonte <klimchek@gmail.com>
2015-09-28 13:59:36 +0300

    sms center send

--- src/Sms/Drivers/SmsCenter.php
+++ src/Sms/Drivers/SmsCenter.php
@@ -1,7 +1,101 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: user
- * Date: 28.09.15
- * Time: 11:56
- */
+<?php namespace SmartCrowd\Sms\Drivers;
+
+use GuzzleHttp\Client;
+use SimpleSoftwareIO\SMS\Drivers\AbstractSMS;
+use SimpleSoftwareIO\SMS\Drivers\DriverInterface;
+use SimpleSoftwareIO\SMS\IncomingMessage;
+use SimpleSoftwareIO\SMS\OutgoingMessage;
+
+class SmsCenter extends AbstractSMS implements DriverInterface
+{
+    /**
+     * The Guzzle HTTP Client
+     *
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+
+    /**
+     * The API's URL.
+     *
+     * @var string
+     */
+    protected $apiBase = 'https://smsc.ru/sys';
+
+    /**
+     * The ending of the URL that all requests must have.
+     *
+     * @var array
+     */
+    protected $apiEnding = ['fmt' => '3'];
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Creates many IncomingMessage objects and sets all of the properties.
+     *
+     * @param $rawMessage
+     * @return mixed
+     */
+    protected function processReceive($rawMessage)
+    {
+        // TODO: Implement processReceive() method.
+    }
+
+    /**
+     * Sends a SMS message
+     *
+     * @param OutgoingMessage $message
+     * @throws \Exception
+     */
+    public function send(OutgoingMessage $message)
+    {
+        $composedMessage = $message->composeMessage();
+
+        $data = [
+            'phones' => $message->getTo(),
+            'mes'    => $composedMessage
+        ];
+
+        $this->buildCall('/send.php');
+        $this->buildBody($data);
+
+        dd($this->postRequest());
+    }
+
+    /**
+     * Checks the server for messages and returns their results.
+     *
+     * @param array $options
+     * @return array
+     */
+    public function checkMessages(Array $options = array())
+    {
+        // TODO: Implement checkMessages() method.
+    }
+
+    /**
+     * Gets a single message by it's ID.
+     *
+     * @param $messageId
+     * @return IncomingMessage
+     */
+    public function getMessage($messageId)
+    {
+        // TODO: Implement getMessage() method.
+    }
+
+    /**
+     * Receives an incoming message via REST call.
+     *
+     * @param $raw
+     * @return \SimpleSoftwareIO\SMS\IncomingMessage
+     */
+    public function receive($raw)
+    {
+        // TODO: Implement receive() method.
+    }
+}
--- src/Sms/SmsServiceProvider.php
+++ src/Sms/SmsServiceProvider.php
@@ -1,6 +1,48 @@
 <?php namespace SmartCrowd\Sms;
 
+use GuzzleHttp\Client;
+use SmartCrowd\Sms\Drivers\SmsCenter;
+
 class SmsServiceProvider extends \SimpleSoftwareIO\SMS\SMSServiceProvider
 {
 
+    public function registerSender()
+    {
+        try {
+
+            return parent::registerSender();
+
+        } catch (\InvalidArgumentException $e) {
+
+            $driver = config('sms.driver');
+
+            switch ($driver) {
+                case 'smsru':
+                    return $this->buildSmsRu();
+
+                case 'smscenter':
+                    return $this->buildSmsCenter();
+
+                default:
+                    throw new \InvalidArgumentException('Invalid SMS driver.', 0, $e);
+            }
+        }
+    }
+
+    protected function buildSmsCenter()
+    {
+        $provider = new SmsCenter(new Client);
+
+        $provider->buildBody([
+            'login' => config('sms.smscenter.login'),
+            'psw'   => config('sms.smscenter.password'),
+        ]);
+
+        return $provider;
+    }
+
+    protected function buildSmsRu()
+    {
+
+    }
 }
