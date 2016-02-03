56cc2128bff5ede0fcd66927cb557d9d293cab6c
yuriy-smolin <y.smolin@smart-crowd.ru>
2016-02-03 15:42:43 +0300

    Added DriverManager

---
+++ src/Sms/DriverManager.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SmartCrowd\Sms;
+
+use Illuminate\Support\Manager;
+use SmartCrowd\Sms\Drivers\SmsRu;
+use SmartCrowd\Sms\Drivers\SmsCenter;
+use SmartCrowd\Sms\Drivers\SmsClub;
+use GuzzleHttp\Client;
+
+class DriverManager extends Manager
+{
+    /**
+     * Get the default sms driver name.
+     *
+     * @return string
+     */
+    public function getDefaultDriver()
+    {
+        return $this->app['config']['sms.driver'];
+    }
+
+    /**
+     * Create an instance of the smsru driver
+     *
+     * @return SmsRu
+     */
+    public function createSmsruDriver(){
+        $config = $this->app['config']->get('sms.smsru', []);
+
+        $driver = new SmsRu(new Client);
+        $driver->buildBody([
+            'api_id' => $config['api_id']
+        ]);
+
+        return $driver;
+    }
+
+    /**
+     * Create an instance of the smscenter driver
+     *
+     * @return SmsCenter
+     */
+    public function createSmscenterDriver(){
+        $config = $this->app['config']->get('sms.smscenter', []);
+
+        $driver = new SmsCenter(new Client);
+        $driver->buildBody([
+            'login' => $config['login'],
+            'psw'   => $config['password'],
+        ]);
+
+        return $driver;
+    }
+
+    /**
+     * Create an instance of the smsclub driver
+     *
+     * @return SmsClub
+     */
+    public function createSmsclubDriver(){
+        $config = $this->app['config']->get('sms.smsclub', []);
+
+        $driver = new SmsClub(new Client);
+        $driver->buildBody([
+            'username'   => $config['login'],
+            'password'   => $config['password'],
+            'lifetime'   => 60 * 24 * 30, // one month
+        ]);
+
+        return $driver;
+    }
+
+}
--- src/Sms/SmsServiceProvider.php
+++ src/Sms/SmsServiceProvider.php
@@ -1,56 +1,13 @@
-<?php namespace SmartCrowd\Sms;
-
-use GuzzleHttp\Client;
-use SmartCrowd\Sms\Drivers\SmsCenter;
-use SmartCrowd\Sms\Drivers\SmsRu;
-use SmartCrowd\Sms\Drivers\SmsClub;
+<?php
+namespace SmartCrowd\Sms;
 
 class SmsServiceProvider extends \SimpleSoftwareIO\SMS\SMSServiceProvider
 {
 
     public function registerSender()
     {
-        parent::registerSender();
-
-        $sender = $this->app['sms.sender'];
-
-        $sender->extend('smsru', function ($app) {
-
-            $config = $app['config']->get('sms.smsru', []);
-
-            $driver = new SmsRu(new Client);
-            $driver->buildBody([
-                'api_id' => $config['api_id']
-            ]);
-
-            return $driver;
-        });
-
-        $sender->extend('smscenter', function ($app) {
-
-            $config = $app['config']->get('sms.smscenter', []);
-
-            $driver = new SmsCenter(new Client);
-            $driver->buildBody([
-                'login' => $config['login'],
-                'psw'   => $config['password'],
-            ]);
-
-            return $driver;
-        });
-
-        $sender->extend('smsclub', function ($app) {
-
-            $config = $app['config']->get('sms.smsclub', []);
-
-            $driver = new SmsClub(new Client);
-            $driver->buildBody([
-                'username'   => $config['login'],
-                'password'   => $config['password'],
-                'lifetime'   => 60 * 24 * 30, // one month
-            ]);
-
-            return $driver;
+        $this->app['sms.sender'] = $this->app->share(function ($app) {
+            return (new DriverManager($app))->driver();
         });
     }
 }
